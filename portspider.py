59c34b5f08db91f8dd6dc7a6b7e72d961c829c83
Jose Machado <jose.machado@finxi.com.br>
2020-11-12 17:09:27 -0300

    feat: search for inputed value of portcall_number

--- portspider.py
+++ portspider.py
@@ -7,19 +7,29 @@ import time
 
 # Copy geckodriver to /var/bin
 
+portcall_sample_value = "20000350812"
 # driver = webdriver.Firefox(executable_path='/')
 driver = webdriver.Firefox()
+driver.set_page_load_timeout(6)  # When to try again and reload.
 driver.get("http://www.mercante.transportes.gov.br/g36127/")
-time.sleep(3)
+time.sleep(2)
+
 elem = driver.find_element_by_link_text("aqui")
 elem.click()
-time.sleep(3)
+time.sleep(2)
 
-frame1 = driver.find_element_by_name("header")
-driver.switch_to.frame(frame1)
-
+frame_select = driver.find_element_by_name("header")
+driver.switch_to.frame(frame_select)
 select1 = Select(driver.find_element_by_tag_name("select"))
 select1_option = select1.select_by_visible_text("Consultar")
 
-time.sleep(10)
+driver.switch_to.default_content()
+frame_portcall_form = driver.find_element_by_id("Principal")
+driver.switch_to.frame(frame_portcall_form)
+elem = driver.find_element_by_name("NumEscala")
+elem.clear()
+elem.send_keys(portcall_sample_value)
+elem.send_keys(Keys.RETURN)
+
+time.sleep(6)
 driver.close()
