c8d6d9f0a2475d7476311877b5f17b941b72abe6
Jose Machado <jose.machado@finxi.com.br>
2020-11-16 17:33:00 -0300

    feat: create endpoint to list portcalls

--- src/main.py
+++ src/main.py
@@ -15,3 +15,14 @@ def get_merchant(portcall_id: str, response: Response):
     except Exception as e:
         response.status_code = 400
         return e
+
+
+@app.get("/merchant/portcalls/{start_date}/{end_date}/{port}")
+def get_portcalls(start_date: str, end_date: str, port: str, response: Response):
+    scraper = MerchantScraper()
+
+    try:
+        return scraper.list_portcalls_by_date(start_date, end_date, port)
+    except Exception as e:
+        response.status_code = 400
+        return e
