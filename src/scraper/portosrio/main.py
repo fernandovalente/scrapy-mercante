8b643d2abe36cca9ec324bbfaae18955e8c98ea5
Breno Gomes <brenodega28@gmail.com>
2020-11-24 16:55:47 -0300

    chore: resolving merge

--- src/scraper/portosrio/main.py
+++ src/scraper/portosrio/main.py
@@ -4,11 +4,7 @@ from bs4 import BeautifulSoup
 
 
 class PortosRio:
-<<<<<<< HEAD
-    def get_data_from_date(date):
-=======
     def get_data_from_date(self, date):
->>>>>>> 9726979200e7753c9d395cd0b6de36f4de628cb8
         url = "http://www.portosrio.gov.br/prognav"
         body = {"data": date, "submit": "enviar"}
 
@@ -16,32 +12,6 @@ class PortosRio:
         soup = BeautifulSoup(response.text)
         data = []
 
-<<<<<<< HEAD
-        table = soup.get_all("table")[0]
-
-        for tr in table.get_all("tbody")[0].get_all("tr"):
-            tds = table.get_all("td")
-
-            data.append(
-                {
-                    "terminal": tds[0],
-                    "local": tds[1],
-                    "procedence": tds[2],
-                    "atrac": tds[3],
-                    "agops": tds[4],
-                    "vessel": tds[5],
-                    "daytime_terno": tds[6],
-                    "daytime_equip": tds[7],
-                    "daytime_operation": tds[8],
-                    "nocturnal_terno": tds[9],
-                    "nocturnal_operation": tds[10],
-                    "nocturnal_equip": tds[11],
-                    "cabeco_start": tds[12],
-                    "cabeco_end": tds[13],
-                    "start": tds[14],
-                    "cargo": tds[15],
-                    "weight": tds[16],
-=======
         table = soup.find_all("table")[0]
 
         for tr in table.find_all("tbody")[0].find_all("tr"):
@@ -66,7 +36,6 @@ class PortosRio:
                     "start": tds[14].text,
                     "cargo": tds[15].text,
                     "weight": tds[16].text,
->>>>>>> 9726979200e7753c9d395cd0b6de36f4de628cb8
                 }
             )
 
